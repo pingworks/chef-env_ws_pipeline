ba616ad11bc1361df11072acffd5691c0d4b6066
Christoph Lukas <lukas@pingworks.de>
2015-10-13 15:43:35 +0200

    fixed jenkins config recipe

--- recipes/pipeline-config.rb
+++ recipes/pipeline-config.rb
@@ -27,7 +27,7 @@ bash 'extract jenkins-config' do
   code 'tar xvz -f /tmp/jenkins-config.tar.gz -C /var/lib/jenkins'
 end
 
-if node['pw_base']['os_user'] == 'pingworks' && node['pw_base']['os_domain'].split('.')[0] == 'prod' then
+if node['ws-env-pipeline']['os_user'] == 'pingworks' && node['pw_base']['domain'].split('.')[0] == 'prod' then
   bash 'extract pingworks-config' do
     user 'jenkins'
     group 'jenkins'
