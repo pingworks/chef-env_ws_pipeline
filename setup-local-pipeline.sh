e54769f2f780b42c37f4840e54cf33d870e59575
Christoph Lukas <lukas@pingworks.de>
2015-11-13 21:29:27 +0100

    separate start-local-pipeline script

--- setup-local-pipeline.sh
+++ setup-local-pipeline.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
 
-domain=local
-
-function get_docker_ip {
-  local name=$1
-  local ip
-
-  echo "$(docker inspect $name \
-    | jq '.[0].NetworkSettings.IPAddress' \
-    | tr -d '"' \
-  )"
-}
-
 set -e
 
 sudo apt-get install -y jq
@@ -26,25 +14,4 @@ sleep 5
 cp -a Berksfile.github Berksfile
 kitchen converge
 
-echo
-echo
-echo "####################################################"
-echo "# Setting up name resolution to work without dns   #"
-echo "#                                                  #"
-echo "####################################################"
-sleep 5
-ip=$(get_docker_ip jkmaster)
-lines="$(grep -vE 'jkmaster|jkslave|dash|git|repo' /etc/hosts)"
-for name in jkmaster dash git repo; do
-  lines="${lines}\n$ip $name.$domain $name"
-done
-ip=$(get_docker_ip jkslave)
-lines="${lines}\n$ip jkslave.$domain jkslave"
-echo -e "$lines" | sudo tee /etc/hosts >/dev/null
-
-for name in jkmaster jkslave; do
-  kitchen login $name <<< "
-    sed -e 's;^\(.*jkmaster\)$;\1 git repo dash;' /etc/hosts > /tmp/hosts.new
-    cat /tmp/hosts.new | sudo tee /etc/hosts >/dev/null
-  "
-done
+./start-local-pipeline.sh
---
+++ start-local-pipeline.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+domain=local
+
+function get_docker_ip {
+  local name=$1
+  local ip
+
+  echo "$(docker inspect $name \
+    | jq '.[0].NetworkSettings.IPAddress' \
+    | tr -d '"' \
+  )"
+}
+
+set -e
+
+[ -x /usr/bin/jq ] || sudo apt-get install -y jq
+
+echo
+echo
+echo "####################################################"
+echo "# Starting pipeline containers                     #"
+echo "#                                                  #"
+echo "####################################################"
+docker start jkmaster
+docker start jkslave
+
+echo
+echo
+echo "####################################################"
+echo "# Setting up name resolution to work without dns   #"
+echo "#                                                  #"
+echo "####################################################"
+sleep 5
+ip=$(get_docker_ip jkmaster)
+lines="$(grep -vE 'jkmaster|jkslave|dash|git|repo' /etc/hosts)"
+for name in jkmaster dash git repo; do
+  lines="${lines}\n$ip $name.$domain $name"
+done
+ip=$(get_docker_ip jkslave)
+lines="${lines}\n$ip jkslave.$domain jkslave"
+echo -e "$lines" | sudo tee /etc/hosts >/dev/null
+
+for name in jkmaster jkslave; do
+  kitchen login $name <<< "
+    sed -e 's;^\(.*jkmaster\)$;\1 git repo dash;' /etc/hosts > /tmp/hosts.new
+    cat /tmp/hosts.new | sudo tee /etc/hosts >/dev/null
+  "
+done
